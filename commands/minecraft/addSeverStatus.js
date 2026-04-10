34fe337cabefca0feebb70310dd28d9ab2bf174c
godprofalco <godprofalco@gmail.com>
2026-04-11 05:27:30 +0600

    Enhance addserverstatus command functionality
    Updated the addserverstatus command to improve descriptions and add permission checks.

--- commands/minecraft/addSeverStatus.js
+++ commands/minecraft/addSeverStatus.js
@@ -3,77 +3,99 @@ const {
   ChannelType,
   EmbedBuilder,
 } = require('discord.js');
-
 const ServerStatus = require('../../models/ServerStatus');
-
-const OWNER_ID = '969181284784025670';
+const serverStatusUpdater = require('../../functions/serverStatusUpdater');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('addserverstatus')
-    .setDescription('Add a Minecraft server')
-
-    .addStringOption(o =>
-      o.setName('servername').setDescription('Server name').setRequired(true)
+    .setDescription('Add a Minecraft server to track its status.')
+    .addStringOption((option) =>
+      option
+        .setName('servername')
+        .setDescription('The name of the server.')
+        .setRequired(true)
     )
-
-    .addStringOption(o =>
-      o.setName('serverip').setDescription('Server IP').setRequired(true)
+    .addStringOption((option) =>
+      option
+        .setName('serverip')
+        .setDescription('The IP address of the server.')
+        .setRequired(true)
     )
-
-    .addStringOption(o =>
-      o.setName('gamemode')
-        .setDescription('Java or Bedrock')
+    .addStringOption((option) =>
+      option
+        .setName('gamemode')
+        .setDescription('The game mode of the server (Java or Bedrock).')
         .setRequired(true)
         .addChoices(
           { name: 'Java', value: 'java' },
           { name: 'Bedrock', value: 'bedrock' }
         )
     )
-
-    .addChannelOption(o =>
-      o.setName('channel')
-        .setDescription('Channel for updates')
+    .addChannelOption((option) =>
+      option
+        .setName('channel')
+        .setDescription('The channel where the server status will be posted.')
         .addChannelTypes(ChannelType.GuildText)
         .setRequired(true)
-    )
-
-    .addBooleanOption(o =>
-      o.setName('hideip')
-        .setDescription('Hide real IP')
-        .setRequired(false)
     ),
-
   async execute(interaction) {
-    if (interaction.user.id !== OWNER_ID) {
-      return interaction.reply({ content: 'Owner only.', ephemeral: true });
+    if (!interaction.member.permissions.has('ManageGuild')) {
+      return interaction.reply({
+        content:
+          'You do not have `ManageGuild` permission to add server status!',
+        ephemeral: true,
+      });
     }
-
     const serverName = interaction.options.getString('servername');
     const serverIp = interaction.options.getString('serverip');
     const gameMode = interaction.options.getString('gamemode');
     const channel = interaction.options.getChannel('channel');
-    const hideIp = interaction.options.getBoolean('hideip') ?? false;
 
-    await ServerStatus.create({
-      guildId: interaction.guild.id,
-      channelId: channel.id,
+    const guildId = interaction.guild.id;
+    const channelId = channel.id;
+
+    const existingEntry = await ServerStatus.findOne({
+      guildId,
+      serverIp,
+      channelId,
+    });
+
+    if (existingEntry) {
+      return interaction.reply({
+        content:
+          'This server is already being tracked in the specified channel!',
+        ephemeral: true,
+      });
+    }
+
+    const newServerStatus = new ServerStatus({
+      guildId,
+      channelId,
       serverName,
       serverIp,
       gameMode,
-      hideIp,
     });
+
+    await newServerStatus.save();
 
     return interaction.reply({
       embeds: [
         new EmbedBuilder()
-          .setColor('#00FF7F')
-          .setTitle('Added')
-          .setDescription(`${serverName} (${serverIp})`)
+          .setColor('#008080')
+          .setTitle('✅ Server Status Tracking Added')
+          .setDescription(
+            `Successfully added server status tracking for **${serverName}** (\`${serverIp}\`, ${gameMode.toUpperCase()}) in <#${channelId}>.`
+          )
           .addFields({
-            name: 'Hide IP',
-            value: hideIp ? 'Yes 🔒' : 'No 🌐',
-          }),
+            name: '⏱ Please Note',
+            value: `The first update will appear shortly. Please wait for the next update cycle!`,
+            inline: false,
+          })
+          .setFooter({
+            text: 'Status tracking will update every 30 seconds.',
+          })
+          .setTimestamp(),
       ],
       ephemeral: true,
     });
