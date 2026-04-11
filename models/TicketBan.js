99613c4df9cb5438b84e276fd96fbcd57d345a68
godprofalco <godprofalco@gmail.com>
2026-04-12 01:35:56 +0600

    Refactor TicketBan schema definition

--- models/TicketBan.js
+++ models/TicketBan.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-const ticketBanSchema = new mongoose.Schema({
+module.exports = mongoose.model('TicketBan', new mongoose.Schema({
   guildId: String,
   userId: String,
   reason: String,
   moderatorId: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('TicketBan', ticketBanSchema);
+}));
