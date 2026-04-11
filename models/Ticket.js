b4a0ab72a959536d3fd12b9fed39f732dac5b6e1
godprofalco <godprofalco@gmail.com>
2026-04-12 01:35:14 +0600

    Refactor ticket schema to include claimed fields

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
 
-const ticketSchema = new mongoose.Schema({
+module.exports = mongoose.model('Ticket', new mongoose.Schema({
   guildId: String,
   channelId: String,
   userId: String,
-
-  category: String,
-  reason: String,
-
+  ticketId: String,
   status: { type: String, default: 'open' },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Ticket', ticketSchema);
+  claimedBy: String,
+  claimedAt: Date,
+}));
