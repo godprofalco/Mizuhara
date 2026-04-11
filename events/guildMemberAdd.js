5b7aebc47956556f0c1f63b71f803ce573d236cf
godprofalco <godprofalco@gmail.com>
2026-04-11 23:31:26 +0600

    Refactor text replacement function in guildMemberAdd

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,6 +3,7 @@ const Welcome = require('../models/welcome');
 
 module.exports = {
   name: 'guildMemberAdd',
+
   async execute(member) {
     const data = await Welcome.findOne({ serverId: member.guild.id });
 
@@ -11,30 +12,26 @@ module.exports = {
     const channel = member.guild.channels.cache.get(data.channelId);
     if (!channel) return;
 
-    const replaced = (text) =>
+    const replace = (text) =>
       text
-        ?.replace(/{user}/g, member.user.username)
+        .replace(/{user}/g, member.user.username)
         .replace(/{mention}/g, `<@${member.user.id}>`)
         .replace(/{tag}/g, member.user.tag)
         .replace(/{id}/g, member.user.id)
         .replace(/{server}/g, member.guild.name)
         .replace(/{membercount}/g, member.guild.memberCount)
-        .replace(/{joindate}/g, `<t:${Math.floor(Date.now()/1000)}:F>`)
-        .replace(/{created}/g, `<t:${Math.floor(member.user.createdTimestamp/1000)}:R>`)
-        .replace(/{avatar}/g, member.user.displayAvatarURL());
+        .replace(/{created}/g, `<t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`);
 
-    // TEXT MODE
     if (data.type === 'text') {
       return channel.send({
-        content: replaced(data.description),
+        content: replace(data.description),
       });
     }
 
-    // EMBED MODE
     const embed = new EmbedBuilder()
-      .setTitle(replaced(data.title))
-      .setDescription(replaced(data.description))
-      .setFooter({ text: replaced(data.footer) })
+      .setTitle(replace(data.title))
+      .setDescription(replace(data.description))
+      .setFooter({ text: replace(data.footer) })
       .setColor(data.color || '#00BFFF');
 
     channel.send({ embeds: [embed] });
