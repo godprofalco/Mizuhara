7b1733ed3f9b7bfc9773981d44ab0d0b1d69bfb2
godprofalco <godprofalco@gmail.com>
2026-04-12 02:05:04 +0600

    Rename TicketSchema to ticketSchema and add ticketId

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const TicketSchema = new mongoose.Schema({
+const ticketSchema = new mongoose.Schema({
   guildId: String,
   channelId: String,
   userId: String,
   reason: String,
   category: String,
   status: { type: String, default: 'open' },
-  createdAt: { type: Date, default: Date.now },
+  ticketId: String,
 });
 
-module.exports = mongoose.model('Ticket', TicketSchema);
+module.exports = mongoose.model('Ticket', ticketSchema);
