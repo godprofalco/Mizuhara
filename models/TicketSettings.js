44997685a38ed2a69ad13216c43d3aab41ad82bc
godprofalco <godprofalco@gmail.com>
2026-04-12 01:42:19 +0600

    Refactor TicketSettings schema definition

--- models/TicketSettings.js
+++ models/TicketSettings.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('TicketSettings', new mongoose.Schema({
-  guildId: String,
+const TicketSettingsSchema = new mongoose.Schema({
+  guildId: { type: String, required: true, unique: true },
   enabled: { type: Boolean, default: true },
   categoryId: String,
   logChannelId: String,
   supportRoleIds: [String],
   ticketLimit: { type: Number, default: 3 },
-}));
+});
+
+module.exports = mongoose.model('TicketSettings', TicketSettingsSchema);
