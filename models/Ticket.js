95ab714ca78322a4fde65f31dfbe6f2aae910772
godprofalco <godprofalco@gmail.com>
2026-04-12 01:41:45 +0600

    Update Ticket.js

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Ticket', new mongoose.Schema({
+const TicketSchema = new mongoose.Schema({
   guildId: String,
   channelId: String,
   userId: String,
-  ticketId: String,
+  reason: String,
+  category: String,
   status: { type: String, default: 'open' },
-  claimedBy: String,
-  claimedAt: Date,
-}));
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Ticket', TicketSchema);
