276756accec4821d73532e9920aa5b5588a83432
godprofalco <godprofalco@gmail.com>
2026-04-12 11:20:00 +0600

    Refactor ticket setup by removing database updates
    Removed TicketPanel and TicketSettings updates from ticket setup process.

--- commands/ticket/ticketsetup.js
+++ commands/ticket/ticketsetup.js
@@ -6,9 +6,6 @@ const {
   ButtonStyle,
 } = require('discord.js');
 
-const TicketPanel = require('../../models/TicketPanel');
-const TicketSettings = require('../../models/TicketSettings');
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ticketsetup')
@@ -16,45 +13,11 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   async execute(interaction) {
-
-    await TicketPanel.findOneAndUpdate(
-      { guildId: interaction.guild.id },
-      {
-        guildId: interaction.guild.id,
-        title: '🎫 Tickets',
-        description: 'Select option',
-        footer: 'Ticket System',
-        dropdowns: [],
-      },
-      { upsert: true }
-    );
-
-    await TicketSettings.findOneAndUpdate(
-      { guildId: interaction.guild.id },
-      {
-        guildId: interaction.guild.id,
-        supportRoleIds: [],
-        adminRoleIds: [],
-      },
-      { upsert: true }
-    );
-
     const row = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setCustomId('setup_add_dropdown')
-        .setLabel('Add Dropdown')
-        .setStyle(ButtonStyle.Success),
-
-      new ButtonBuilder()
-        .setCustomId('setup_roles')
-        .setLabel('Set Roles')
-        .setStyle(ButtonStyle.Primary)
+      new ButtonBuilder().setCustomId('setup_add_dropdown').setLabel('Add Dropdown').setStyle(ButtonStyle.Success),
+      new ButtonBuilder().setCustomId('setup_roles').setLabel('Set Roles').setStyle(ButtonStyle.Primary)
     );
 
-    await interaction.reply({
-      content: '⚙️ Setup Panel Opened',
-      components: [row],
-      ephemeral: true,
-    });
+    interaction.reply({ content: '⚙️ Setup UI', components: [row], ephemeral: true });
   },
 };
