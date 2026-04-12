034e46cc46478a49caf956a26d944330e30c1187
godprofalco <godprofalco@gmail.com>
2026-04-12 11:20:26 +0600

    Refactor createpanel.js for cleaner syntax

--- commands/ticket/createpanel.js
+++ commands/ticket/createpanel.js
@@ -11,29 +11,22 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('createpanel')
     .setDescription('Send ticket panel')
-    .addChannelOption(o =>
-      o.setName('channel').setRequired(true)
-    ),
+    .addChannelOption(o => o.setName('channel').setRequired(true)),
 
   async execute(interaction) {
     const panel = await TicketPanel.findOne({ guildId: interaction.guild.id });
 
     const menu = new StringSelectMenuBuilder()
       .setCustomId('ticket_menu')
-      .setPlaceholder('Select Ticket Type')
-      .addOptions(
-        panel.dropdowns.map(d => ({
-          label: d.name,
-          value: d.name,
-          emoji: d.emoji,
-          description: d.description,
-        }))
-      );
+      .addOptions(panel.dropdowns.map(d => ({
+        label: d.name,
+        value: d.name,
+        emoji: d.emoji,
+      })));
 
     const embed = new EmbedBuilder()
       .setTitle(panel.title)
-      .setDescription(panel.description)
-      .setFooter({ text: panel.footer });
+      .setDescription(panel.description);
 
     await interaction.options.getChannel('channel').send({
       embeds: [embed],
