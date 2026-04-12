60f8c517fc7d61ee183b816d8cb875b410932683
godprofalco <godprofalco@gmail.com>
2026-04-12 11:12:38 +0600

    Refactor TicketSettings model export syntax

--- models/TicketSettings.js
+++ models/TicketSettings.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model(
-  'TicketSettings',
-  new mongoose.Schema({
-    guildId: String,
-    supportRoleIds: [String],
-    adminRoleIds: [String],
-    logChannelId: String,
-    ticketCounter: { type: Number, default: 0 },
-  })
-);
+module.exports = mongoose.model('TicketSettings', new mongoose.Schema({
+  guildId: String,
+  supportRoleIds: [String],
+  adminRoleIds: [String],
+  logChannelId: String,
+  ticketCounter: { type: Number, default: 0 },
+}));
