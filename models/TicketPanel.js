c2a4e0488d0704cd58f2e6c51ce0dcaa50f6c4db
godprofalco <godprofalco@gmail.com>
2026-04-12 11:13:22 +0600

    Update TicketPanel.js

--- models/TicketPanel.js
+++ models/TicketPanel.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
 
-const dropdownSchema = new mongoose.Schema({
-  name: String,
-  emoji: String,
+module.exports = mongoose.model('TicketPanel', new mongoose.Schema({
+  guildId: String,
+  title: String,
   description: String,
-  categoryId: String,
-});
-
-module.exports = mongoose.model(
-  'TicketPanel',
-  new mongoose.Schema({
-    guildId: String,
-    title: String,
-    description: String,
-    footer: String,
-    dropdowns: [dropdownSchema],
-  })
-);
+  footer: String,
+  dropdowns: [
+    {
+      name: String,
+      emoji: String,
+      description: String,
+      categoryId: String,
+    },
+  ],
+}));
