48320476e1ff95e960c02998426f6f0c886956ab
godprofalco <godprofalco@gmail.com>
2026-04-12 11:13:48 +0600

    Add moderatorId field to TicketBan schema

--- models/TicketBan.js
+++ models/TicketBan.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model(
-  'TicketBan',
-  new mongoose.Schema({
-    guildId: String,
-    userId: String,
-    reason: String,
-  })
-);
+module.exports = mongoose.model('TicketBan', new mongoose.Schema({
+  guildId: String,
+  userId: String,
+  moderatorId: String,
+  reason: String,
+}));
