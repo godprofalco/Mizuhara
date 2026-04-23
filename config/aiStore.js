c771d27dc95b778bf2b445fea9da19c5d965c527
Godpro Falco <godprofalco@gmail.com>
2026-04-23 20:54:23 +0600

    Delete config directory

--- config/aiStore.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globalPrompt: "You are a helpful AI assistant.",
-
-  guildPrompts: new Map(),      // server prompts
-  activeChannels: new Map(),    // active AI channels per server
-};
